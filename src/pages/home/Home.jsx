ce517823908df924eb464b51728d410138bb65d2
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-30 14:30:58 +0300

    Fiverr App

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.scss";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
