c9c1d558a9738425b7d6825b10f4e12bedd0de57
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-30 14:27:11 +0300

    Fiverr App

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Featured.scss";
+
+const Featured = () => {
+  return <div></div>;
+};
+
+export default Featured;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import "./Navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+  const { pathname } = useLocation();
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -25,7 +26,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className={active ? "navbar active" : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <Link to="/" className="link">
@@ -74,40 +75,39 @@ const Navbar = () => {
           )}
         </div>
       </div>
-      {active && (
+      {(active || pathname !== "/") && (
         <>
           <hr />
           <div className="menu">
-            {/* <Link className="link menuLink" to="/">
-          Graphics & Design
-        </Link>
-        <Link className="link menuLink" to="/">
-          Video & Animation
-        </Link>
-        <Link className="link menuLink" to="/">
-        Writing & Translation
-        </Link>
-        <Link className="link menuLink" to="/">
-        AI Services
-        </Link>
-        <Link className="link menuLink" to="/">
-        Digital Marketing
-        </Link>
-        <Link className="link menuLink" to="/">
-        Music & Audio
-        </Link>
-        <Link className="link menuLink" to="/">
-          Programming & Tech
-        </Link>
-        <Link className="link menuLink" to="/">
-          Business
-        </Link>
-        <Link className="link menuLink" to="/">
-          Lifestyle
-        </Link> */}
-            <span>ylm</span>
-            <span>ylm2</span>
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
           </div>
+          <hr />
         </>
       )}
     </div>
