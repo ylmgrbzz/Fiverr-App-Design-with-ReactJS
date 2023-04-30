8b9b5c84358dc077bceb0167d290eb631729dab0
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-30 16:57:38 +0300

    Fiverr App

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ import { cards } from "../../data";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Featured />
       <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5}>
@@ -16,6 +16,48 @@ const Home = () => {
           <CatCard item={card} key={card.id} />
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
